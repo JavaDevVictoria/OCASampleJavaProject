b8ecf8db964f73e96809e9fa725a7d3f44e4fba9
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2023-02-09 14:09:57 +0000

    Added static variable and static initializer

--- Employee.java
+++ Employee.java
@@ -6,11 +6,19 @@ class Employee {
     private String name;
     private int id;
     private LocalDate dob;
+	
+	static int count = 0;
+	
+	// Static initializer
+	static {
+		System.out.println("Employee class loaded");
+	}
 
     public Employee(String name, int id, LocalDate dob) {
         this.name = name;
         this.id = id;
         this.dob = dob;
+		count++;
     }
 
     public String getName() {
